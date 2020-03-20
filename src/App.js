eac717e25bb023c7b843052eaf40a06f2ee158b1
muqsith <muqsithck@gmail.com>
2020-03-21 01:57:11 +0530

    ui fixes

--- src/App.js
+++ src/App.js
@@ -5,156 +5,268 @@ import "./index.css";
 import countryCode from "./code";
 
 export default function App() {
-  const [coronaData, setCoronaData] = useState([]);
+  const [countryData, setCountryData] = useState([]);
+  const [worldData, setWorldData] = useState({});
+  const [date, setDate] = useState(1584724653093);
   const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios.get("https://corona.lmao.ninja/countries").then(res => {
+      setCountryData([...res.data]);
+    });
+
+    axios.get("https://corona.lmao.ninja/all").then(res => {
+      console.log("res", res.data);
+      setWorldData(res.data);
+      setDate(res.data.updated);
+    });
+  }, []);
 
   const handleChnage = e => {
     setSearch(e.target.value);
   };
 
-  useEffect(() => {
-    axios.get("https://corona.lmao.ninja/countries").then(res => {
-      setCoronaData([...res.data]);
-    });
-  }, []);
-
-  let countryList = coronaData
+  let data = countryData;
+  var updatedDate = new Date(date)
+  var day = updatedDate.getDate() + '-' + (updatedDate.getMonth() + 1) + '-' + updatedDate.getFullYear();
+  var time = updatedDate.getHours() + ' : ' + updatedDate.getMinutes();
 
   if (search.length > 0) {
-    let searchResult = coronaData.filter(item =>
+    let searchResult = countryData.filter(item =>
       item.country.toLowerCase().includes(search.toLowerCase())
     );
-    countryList = searchResult;
+    data = searchResult;
   } else {
-    countryList = coronaData;
+    data = countryData;
   }
 
-
-
   return (
-    <Container maxWidth="lg" className="app-container"  style={{ padding: "50px" }}>
-      <Grid container>
-        <Grid item md={4}>
-          <h1 className="heading">COVID19 </h1>
-          <h1 className="heading-main"> UPDATES </h1>
-        </Grid>
-        <Grid item md={8}></Grid>
+    <Container
+      maxWidth="lg"
+      className="app-container"
+      style={{ padding: "50px" }}
+    >
+      {countryData.length > 0 ? (
+        <Grid container>
+          <Grid item md={4} xs={12}>
+            <h1 className="heading-one">COVID19 </h1>
+            <h1 className="heading-two"> UPDATES </h1>
+          </Grid>
 
-        <Grid item md={12}>
-          <input
-            className="input-box"
-            placeholder="Search Country...."
-            onChange={handleChnage}
-          />
-        </Grid>
+          <Grid
+            item
+            md={8}
+            xs={12}
+            style={{
+              display: "flex",
+              alignItems: "flex-end"
+            }}
+          >
+            <Grid container>
+              <Grid item md={3} xs={4}>
+                <div className="world-data-item">
+                  <div>
+                    <p
+                      className="caption"
+                      style={{ color: "#2980b9" }}
+                    >
+                      Total Cases
+                    </p>
+                    <p className="title-large" style={{ color: "#2980b9" }}>
+                      {worldData.cases}
+                    </p>
+                  </div>
+                </div>
+              </Grid>
+              <Grid item md={3} xs={4}>
+                <div className="world-data-item">
+                  <div>
+                    <p
+                      className="caption"
+                      style={{ color: "#27ae60" }}
+                    >
+                      Recovery Rate
+                    </p>
+                    <p className="title-large" style={{ color: "#27ae60" }}>
+                      {(
+                        Math.round(
+                          ((100 * worldData.recovered) /
+                            worldData.cases) *
+                            100
+                        ) / 100
+                      ).toFixed(2)}{" "}
+                      %
+                    </p>
+                  </div>
+                </div>
+              </Grid>
 
-        <Grid item md={12} xs={12} className="heading-wrapper" >
-          <Grid container>
-            <Grid item xs={12} md={4} className="flex-start heading-wrapper">
-              <p className="item-count-heading">Country</p>
-            </Grid>
-
-            <Grid item md={2} xs={3} className="flex-start"
-           
-            >
-              <p className="item-count-heading">Cases</p>
-            </Grid>
-
-            <Grid item md={2} xs={3} className="flex-start">
-              <p className="item-count-heading">Recovered</p>
-            </Grid>
-
-            <Grid item md={2} xs={3} className="flex-start">
-              <p className="item-count-heading">Recovery Rate</p>
-            </Grid>
-
-            <Grid item md={2} xs={3} className="flex-start">
-              <p className="item-count-heading">Death Rate</p>
+              <Grid item md={3} xs={4}>
+                <div className="world-data-item">
+                  <div>
+                    <p
+                      className="caption"
+                      style={{ color: "#e74c3c" }}
+                    >
+                      Death Rate
+                    </p>
+                    <p className="title-large" style={{ color: "#e74c3c" }}>
+                      {Math.round(
+                        ((100 * worldData.deaths) / worldData.cases) *
+                          100
+                      ) / 100}{" "}
+                      %
+                    </p>
+                  </div>
+                </div>
+              </Grid>
             </Grid>
           </Grid>
-        </Grid>
 
+          <Grid item md={12}>
+            <input
+              className="input-box"
+              placeholder="Search Country...."
+              onChange={handleChnage}
+            />
+          </Grid>
 
+          <Grid item md={12} xs={12} className="heading-wrapper">
+            <Grid container>
+              <Grid item xs={12} md={4} className="flex-start">
+                <p className="caption">Country</p>
+              </Grid>
 
+              <Grid item md={2} xs={3} className="flex-start">
+                <p className="caption">Cases</p>
+              </Grid>
 
-        {countryList.length > 0 ? (
-          countryList.map(item => {
-            let recoveredRate =  (Math.round((100 * item.recovered) / item.cases * 100) / 100).toFixed(2)
-            let deathsRate = (Math.round((100 * item.deaths) / item.cases * 100) / 100).toFixed(2)
-          
-            
-            
+              <Grid item md={2} xs={3} className="flex-start">
+                <p className="caption">Recovered</p>
+              </Grid>
 
+              <Grid item md={2} xs={3} className="flex-start">
+                <p className="caption">Recovery Rate</p>
+              </Grid>
 
-            const nationCode = countryCode.find(name => {
-              return name.Name === item.country;
-            });
+              <Grid item md={2} xs={3} className="flex-start">
+                <p className="caption">Death Rate</p>
+              </Grid>
+            </Grid>
+          </Grid>
 
-            let image = `https://www.countryflags.io/${
-              nationCode ? nationCode.Code : null
-            }/shiny/64.png`;
-            
+          {data.length > 0 ? (
+            data.map(item => {
+              let recoveredRate = (
+                Math.round(((100 * item.recovered) / item.cases) * 100) / 100
+              ).toFixed(2);
+              let deathsRate = (
+                Math.round(((100 * item.deaths) / item.cases) * 100) / 100
+              ).toFixed(2);
+              const nationCode = countryCode.find(name => {
+                return name.Name === item.country;
+              });
 
-            return (
-              <Grid item md={12}>
-                <Grid container>
-                  <Grid item xs={12} md={4} className="flex-start">
-                    <img
-                      src={image}
-                      width="50px"
-                      alt="flag"
-                      style={{ margin: "3px", marginRight: "20px" }}
-                    />
-                    <p className="item-count-text">{item.country}</p>
-                  </Grid>
-                  <Grid item xs={3} md={2} className="flex-start">
-                    <div className="item-count">
-                      <div>
-                      <p className="item-count-heading text-hide">Cases</p>
-                      <p className="item-count-text">{item.cases}</p>
-                    </div>
-                    </div>
-                  </Grid>
-                  <Grid item xs={3} md={2} className="flex-start">
-                    <div className="item-count">
-                    <div>
-                      <p className="item-count-heading text-hide">Recovered</p>
-                      <p className="item-count-text">{item.recovered}</p>{" "}
+              let image = `https://www.countryflags.io/${
+                nationCode ? nationCode.Code : null
+              }/shiny/64.png`;
+
+              return (
+                <Grid item md={12}>
+                  <Grid container>
+                    <Grid item xs={12} md={4} className="flex-start">
+                      <img
+                        src={image}
+                        width="50px"
+                        alt="flag"
+                        style={{ margin: "3px", marginRight: "20px" }}
+                      />
+                      <p className="titile">{item.country}</p>
+                    </Grid>
+                    <Grid item xs={3} md={2} className="flex-start">
+                      <div className="country-data-item">
+                        <div>
+                          <p className="caption text-hide">Cases</p>
+                          <p className="titile">{item.cases}</p>
+                        </div>
                       </div>
-                    </div>
-                  </Grid>
-                  <Grid item xs={3} md={2} className="flex-start">
-                    <div
-                      className="item-count"
-                      style={{ backgroundColor: "#27ae60" }}
-                    >
-                       <div>
-                      <p className="item-count-heading text-hide">Recovery</p>
-                      <p className="item-count-text">{recoveredRate} %</p>{" "}
-                    </div>
-                    </div>
-                  </Grid>
-                  <Grid item xs={3} md={2} className="flex-start">
-                    <div
-                      className="item-count"
-                      style={{ backgroundColor: "#e74c3c" }}
-                    >
-                      <div>
-                      <p className="item-count-heading text-hide">Death</p>
-                      <p className="item-count-text">{deathsRate} %</p>
+                    </Grid>
+                    <Grid item xs={3} md={2} className="flex-start">
+                      <div className="country-data-item">
+                        <div>
+                          <p className="caption text-hide">
+                            Recovered
+                          </p>
+                          <p className="titile">{item.recovered}</p>{" "}
+                        </div>
                       </div>
-                    </div>
+                    </Grid>
+                    <Grid item xs={3} md={2} className="flex-start">
+                      <div
+                        className="country-data-item"
+                        style={{ backgroundColor: "#27ae60" }}
+                      >
+                        <div>
+                          <p className="caption text-hide">
+                            Recovery
+                          </p>
+                          <p className="titile">{recoveredRate} %</p>{" "}
+                        </div>
+                      </div>
+                    </Grid>
+                    <Grid item xs={3} md={2} className="flex-start">
+                      <div
+                        className="country-data-item"
+                        style={{ backgroundColor: "#e74c3c" }}
+                      >
+                        <div>
+                          <p className="caption text-hide">Death</p>
+                          <p className="titile">{deathsRate} %</p>
+                        </div>
+                      </div>
+                    </Grid>
                   </Grid>
                 </Grid>
-              </Grid>
-            );
-          })
-        ) : (
-          <p>loading</p>
-        )}
+              );
+            })
+          ) : (
+            <p>loading</p>
+          )}
 
-        <div className="app-wrapper"></div>
-      </Grid>
+          <Grid
+            item
+            xs={12}
+            md={12}
+            className="footer"
+          >
+            <div>
+            <p className="footer-text">
+            Source from worldometers.info
+            </p>
+            <p className="footer-text">
+          Last update at
+            {"   "}
+             
+            {time}
+            {"   "} 
+             &
+            {"   "}
+                
+            {day} 
+            </p>
+            <p className="footer-text">
+            Created  by <a target="blank" href="http://linkedin.com/in/muqsithck" >  Abdul Muqsith</a>
+            </p>
+            </div>
+          </Grid>
+
+          <div className="app-wrapper"></div>
+        </Grid>
+      ) : (
+        <Grid className="loading-screen">
+          <img src={require("./assets/loading.gif")} alt="gif" width="40px" />
+        </Grid>
+      )}
     </Container>
   );
 }
