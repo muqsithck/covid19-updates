ec672d7d99d4236754242539ab85917034dcc79e
muqsith <muqsithck@gmail.com>
2020-03-20 20:15:43 +0530

    Updates

---
+++ precache-manifest.da30435a702d3f03a281ba52c0c00c15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00998f059277eb1e6c0d874be65b6edd",
+    "url": "/covid19-updates/index.html"
+  },
+  {
+    "revision": "2cea2c435b1d4b4c4993",
+    "url": "/covid19-updates/static/css/main.cf2da259.chunk.css"
+  },
+  {
+    "revision": "1be7f9a2a9c45dd64545",
+    "url": "/covid19-updates/static/js/2.e97c11d9.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/covid19-updates/static/js/2.e97c11d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cea2c435b1d4b4c4993",
+    "url": "/covid19-updates/static/js/main.735c8b3a.chunk.js"
+  },
+  {
+    "revision": "4b1d686fe605e641da97",
+    "url": "/covid19-updates/static/js/runtime-main.9c4d536c.js"
+  },
+  {
+    "revision": "7eda9abe2835430537d07a6fc757bd49",
+    "url": "/covid19-updates/static/media/outbreak-coronavirus-world-1024x506px.7eda9abe.jpg"
+  }
+]);
